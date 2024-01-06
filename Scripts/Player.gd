ae12ce9327d703d45d98d314994b8870bc0abea0
Isaac RF <IsaacRF239@gmail.com>
2024-01-06 18:24:03 +0100

    Add collision to tiles

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,15 +2,13 @@ extends CharacterBody2D
 
 
 var move_speed : float = 300.0
-var jump_force : float = 400.0
+var jump_force : float = 200.0
 var gravity : float = 500.0
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	velocity.x = 0
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
