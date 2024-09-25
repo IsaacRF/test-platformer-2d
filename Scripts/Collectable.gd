dae4d54d43aabb1c90c5042364a2ac3eb95e1f26
Isaac RF <IsaacRF239@gmail.com>
2024-09-25 20:38:43 +0200

    Add a Sound Manager
    - Add a Sound Manager to play sounds regardless of emitter status
    - Remove individual SFX player from collectable objects, as sound isn't played on queue_free()

--- Scripts/Collectable.gd
+++ Scripts/Collectable.gd
@@ -5,18 +5,31 @@ extends Area2D
 @export var texture: Texture
 @export var sfx: AudioStream
 
+var bob_height : float = 5.0
+var bob_speed : float = 5.0
+
+@onready var start_y : float = global_position.y
+var t : float = 0.0
+
 func _ready():
 	$Sprite2D.texture = texture
-	$SFX.stream = sfx
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	t += delta
+	
+	var d = (sin(t * bob_speed) + 1) / 2
+	global_position.y = start_y + (d * bob_height)
+
 
 func _on_body_entered(body):
 	if body.is_in_group("Players"):
-		$SFX.play()
+		SoundManager.stream = sfx
+		SoundManager.play()
+		
 		if score_value > 0:
 			body.call("add_score", score_value)
 		if life_value > 0:
 			body.call("add_life", life_value)
+			
+		queue_free()
