602c3fa042c1f41736d9e26b436a3dae506be23e
Isaac RF <IsaacRF239@gmail.com>
2024-10-07 18:00:23 +0200

    Add animations for player and enemies
    - Also add jump sfx

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,12 +4,16 @@ extends CharacterBody2D
 @export var move_speed : float = 30.0
 @export var move_range : Vector2
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
+
 var start_pos : Vector2
 var target_pos : Vector2
 
 func _ready():
 	start_pos = global_position
 	target_pos = start_pos + move_range
+	_animated_sprite.play("idle")
 
 func _process(delta):
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
@@ -19,6 +23,11 @@ func _process(delta):
 			target_pos = start_pos + move_range
 		else:
 			target_pos = start_pos
+			
+	if global_position < target_pos:
+		_animated_sprite.flip_h = true
+	else:
+		_animated_sprite.flip_h = false
 
 func _on_hurtbox_body_entered(body):
 	if body.is_in_group("Players"):
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 class_name Player
 
 @onready var score_label : Label = get_tree().current_scene.get_node("UI/Score")
+@onready var _animated_sprite = $AnimatedSprite2D
+@onready var _death_sfx = $DeathSFX
+@onready var _jump_sfx = $JumpSFX
 
 var move_speed : float = 300.0
 var acceleration : float = 20.0
@@ -11,31 +14,45 @@ var gravity : float = 500.0
 
 var is_dead : bool
 
-var death_sfx : AudioStreamPlayer2D
-
 var score : int
 var life : int
 
 func _ready():
-	death_sfx = $DeathSFX
+	pass
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = -jump_force
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	if not is_dead:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			_jump_sfx.play()
+			_animated_sprite.play("jump")
+			velocity.y = -jump_force
+		
 		var direction = Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = move_toward(velocity.x, direction * move_speed, acceleration)
+			
+			if velocity.x > 0:
+				_animated_sprite.flip_h = true
+			else:
+				_animated_sprite.flip_h = false
+				
+			if is_on_floor():
+				_animated_sprite.play("run")
+			else:
+				_animated_sprite.play("jump")
+				
 		else:
 			velocity.x = move_toward(velocity.x, 0, deceleration)
+			
+			if velocity.x == 0 && velocity.y == 0:
+				_animated_sprite.play("idle")
 	
 	move_and_slide()
 	
@@ -51,8 +68,8 @@ func add_life(amount):
 
 func game_over():
 	is_dead = true
-	death_sfx.play()
-	$Sprite.flip_v = true
+	_death_sfx.play()
+	_animated_sprite.play("death")
 	$CameraFollow.update_position = false
 	$CollisionShape2D.set_deferred("disabled", true)
 	await get_tree().create_timer(1.0).timeout
